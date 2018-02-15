a86223c38e2ba45d1dc80f0d69537b4c6a27a37d
Nathan Lamothe <e1436741@cmaisonneuve.qc.ca>
2018-02-15 10:04:02 -0500

    Detruire id fonctionne ex6 NathanL

--- exercice6.js
+++ exercice6.js
@@ -34,16 +34,32 @@ app.get('/formulaire', (req, res) => {
  console.log('la route get / = ' + req.url)
  res.sendFile(__dirname + "/public/html/forme.htm")
 })
-
+/*
 app.get('/detruire/:telephone', (req, res) => {
-	db.collection('adresse')
-	.findOneAndDelete( {'telephone': req.params.telephone} ,(err, resultat) => {
+	db.collection('adresse').findOneAndDelete( {'telephone': req.params.telephone} ,(err, resultat) => {
 		if (err) return res.send(500, err)
 			var cursor = db.collection('adresse').find().toArray(function(err, resultat){
 		if (err) return console.log(err)
-			res.render('gabarit.ejs', {adresse: resultat})
+			res.render('index.ejs', {adresses: resultat})
 		})
 	}) 
+})
+*/
+
+app.get('/detruire/:id', (req, res) => {
+ var id = req.params.id 
+// var critere = 'ObjectId("58bae3feaf5a674b240cfe53")'
+// 58bae3feaf5a674b240cfe53
+// var critere = ObjectID.createFromHexString(id)
+var critere = ObjectID(req.params.id)
+console.log(critere)
+
+console.log(id)
+ db.collection('adresse').findOneAndDelete({"_id": critere}, (err, resultat) => {
+
+if (err) return console.log(err)
+ res.render('index.ejs', {adresses: resultat})
+ })
 })
 
 app.post('/adresse', (req, res) => {
